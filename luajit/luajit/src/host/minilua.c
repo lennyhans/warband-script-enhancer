fb3a247873322e6c8105fb397b79a910a1a57fa0
kumpu <kumpuu@web.de>
2017-06-06 08:40:54 +0200

    fixed build bug that happened on k700's machine

--- luajit/luajit/src/host/minilua.c
+++ luajit/luajit/src/host/minilua.c
@@ -678,7 +678,9 @@ static FILE *fopenInUserDir(lua_State *L, const char *filename, const char *mode
 	{
 		char *safePath = makeSafePath(L->userDir, filename);
 		if (!safePath)
+		{
 			return NULL;
+		}
 
 		FILE *res = fopen(safePath, mode);
 		free(safePath);
